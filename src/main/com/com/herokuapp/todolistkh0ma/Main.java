739c79682645c3e69f417e1314dcaa3ca8817b91
kh0ma <khomenko.dp@gmail.com>
2017-01-22 20:26:46 +0200

    pom.xml spring jdbc, junit test, logging dependencies was added

---
+++ src/main/com/com/herokuapp/todolistkh0ma/Main.java
@@ -0,0 +1,11 @@
+package com.herokuapp.todolistkh0ma;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main{
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+    public static void main(String[]args){
+        LOG.info("Hello World!");
+    }
+}
